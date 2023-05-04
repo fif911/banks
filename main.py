e3c5b13ddbf5da674efb12c36432e7c49d450e92
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-05-04 15:01:45 +0200

    feat: add deposit and withdrawal

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import enum
 import uuid
 from typing import List, Optional
 
+CURRENT_TIME = 0  # current time in months
+
 
 class UserStatusSavingEnum(enum.Enum, str):
+    UNPAID_LOANS = "UNPAID_LOANS"  # start draining savings account
     LOCKED = "LOCKED"
     ACTIVE = "ACTIVE"
 
@@ -32,6 +35,25 @@ class User:
         self.status = UserStatusSavingEnum.ACTIVE
 
     def add_loan(self, loan: Loan):
+        if self.status == UserStatusSavingEnum.LOCKED:
+            raise ValueError("User is locked")
+        if self.status == UserStatusSavingEnum.UNPAID_LOANS:
+            raise ValueError("User has unpaid loans")
         if len(self.loans) > 3:
             raise ValueError("User can not have more than 3 loans concurrently")
         self.loans.append(loan)
+
+    def withdraw_savings(self, amount):
+        if self.status == UserStatusSavingEnum.LOCKED:
+            raise ValueError("User is locked")
+        if self.status == UserStatusSavingEnum.UNPAID_LOANS:
+            raise ValueError("User has unpaid loans")
+        if amount > self.saving:
+            raise ValueError("Not enough money in savings account")
+        self.saving -= amount
+        print(f"Withdrawn ${amount} from savings account. Current savings: ${self.saving}")
+
+    def deposit_savings(self, amount):
+        if self.status == UserStatusSavingEnum.LOCKED:
+            raise ValueError("User is locked")
+        self.saving += amount
