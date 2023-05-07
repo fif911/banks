42bee7322ef2f88eaf5f73428be3ee8a7b82206b
Oleksandr Zakotianskyi <o.zakotianskyi@student.vu.nl>
2023-05-08 00:52:38 +0200

    feat: implement loan payment and pretty printing, add loan status, enhance user info messsages

--- entities.py
+++ entities.py
@@ -4,6 +4,8 @@ from typing import List, Optional
 
 from faker import Faker
 
+from utils import IOUtils
+
 
 class Session:
     current_time: int  # current time in months
@@ -24,28 +26,42 @@ class Session:
 
 
 class UserStatusSavingEnum(str, enum.Enum):
-    OVERDUE_LOANS = "OVERDUE_LOANS"  # start draining savings account
-    LOCKED = "LOCKED"
+    OVERDUE_LOANS = "OVERDUE_LOANS"  # user has not paid a loan within 12 month - start draining savings account
+    LOCKED = "LOCKED"  # user cannot perform any actions except for logging out
+    ACTIVE = "ACTIVE"  # default status
+
+
+class LoanStatusEnum(str, enum.Enum):
     ACTIVE = "ACTIVE"
+    PAID = "PAID"
 
 
 class Loan:
-    sum: int
+    """Loan entity
+
+    Is issued for 12 months. After 12 months, the loan is expired.
+    Each user may have up to 3 loans.
+    Each loan has an interest rate of 10% for loans up to €2000 and 10.5% for loans equal or above €2000.
+    The loan is represented as a float number with max 2 decimal places, and is rounded to the nearest cent if needed.
+    """
+    sum: float
     initiated_at: int
     interest_rate: float
+    status: LoanStatusEnum
 
-    def __init__(self, amount: int, initiated_at: int):
+    def __init__(self, amount: float, initiated_at: int):
         if amount <= 0:
             raise ValueError("Loan amount can not be negative or zero")
         if amount > 10_000:
             raise ValueError("Bank do not give loans more than €10000")
 
-        self.sum = amount
+        self.sum = IOUtils.round_float_to_2_decimal_places(amount)
         self.initiated_at = initiated_at
         if amount >= 2_000:
             self.interest_rate = 0.105  # 10.5%
         else:
             self.interest_rate = 0.1  # 10%
+        self.status = LoanStatusEnum.ACTIVE
 
     def is_expired(self, session: Session) -> bool:
         """
@@ -55,9 +71,37 @@ class Loan:
         """
         return self.initiated_at + 12 <= session.current_time
 
+    def expires_in(self, session: Session) -> int:
+        """Returns the number of months until the loan expires"""
+        expires_in = self.initiated_at + 12 - session.current_time
+        return expires_in if expires_in > 0 else 0
+
+    def pay(self, amount: float) -> LoanStatusEnum:
+        """Allow user to pay for a loan and returns the loan status"""
+        amount = IOUtils.round_float_to_2_decimal_places(amount)
+        if not amount > 0:
+            raise ValueError("Payment amount can not be negative or zero")
+        self.sum -= amount
+
+        if self.sum <= 0:
+            self.sum = 0
+            self.status = LoanStatusEnum.PAID
+            print("Loan is paid in full and is closed. Thank you!")
+
+        return self.status
+
+    def pretty_print_loan(self, session: Session, prefix: str = ""):
+        loan_print = prefix + f"Loan: €{self.sum}, initiated at: {self.initiated_at}, " \
+                              f"interest_rate {self.interest_rate}, expired: {self.is_expired(session)}"
+        # add the expiration date if the loan is not expired
+        loan_print = loan_print + f", expires in: {self.expires_in(session)}" \
+            if self.is_expired(session) is False else loan_print
+        print(loan_print)
+
     @staticmethod
-    def issue_loan(amount, ) -> "Loan":
-        pass
+    def issue_loan(session: Session, loan_amount: float):
+        """Loan factory method"""
+        return Loan(loan_amount, session.current_time)
 
     def __repr__(self):
         return f"Loan(sum={self.sum}, initiated_at={self.initiated_at}, interest_rate={self.interest_rate})"
@@ -67,7 +111,7 @@ class User:
     username: str
     full_name: str
     loans: List[Loan]
-    saving: int
+    saving: float
     status: UserStatusSavingEnum
 
     def __init__(self, session: Session, loans: Optional[Loan] = None, saving: int = 0):
@@ -87,7 +131,7 @@ class User:
             raise ValueError("User can not have more than 3 loans concurrently")
         self.loans.append(loan)
 
-    def withdraw_savings(self, amount):
+    def withdraw_savings(self, amount: float):
         if self.status == UserStatusSavingEnum.LOCKED:
             raise ValueError("User is locked")
         if self.status == UserStatusSavingEnum.OVERDUE_LOANS:
@@ -97,10 +141,12 @@ class User:
         self.saving -= amount
         print(f"Withdrawn €{amount} from savings account. Current savings: ${self.saving}")
 
-    def deposit_savings(self, amount):
+    def deposit_savings(self, deposit_amount: float):
         if self.status == UserStatusSavingEnum.LOCKED:
             raise ValueError("User is locked")
-        self.saving += amount
+        self.saving += deposit_amount
+        print(f"Deposited €{deposit_amount}. Current savings: €{self.saving}")
 
     def __repr__(self):
-        return f"User(username={self.username}, full_name={self.full_name}, loans={self.loans}, saving=€{self.saving}, status={self.status})"
+        return f"User(username={self.username}, full_name={self.full_name}, loans={self.loans}, " \
+               f"saving=€{self.saving}, status={self.status})"
