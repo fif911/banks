15631c32f7d84c32ff795fe24625826caf4efa85
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-05-09 20:06:48 +0200

    feat: bugfix taking loan for loans overdue customer; add notification center; add notification on next month rate adjustment

--- user_panel.py
+++ user_panel.py
@@ -6,6 +6,36 @@ from utils import IOUtils
 USER_MENU_PROMPT_MESSAGE = "Choose the action from the Main User Menu: "
 
 
+def print_user_dashboard(user: User, session: Session):
+    print("-" * 10 + " Your dashboard " + "-" * 10)
+    print("Your " + str(user.savings_account))
+    if user.loans:
+        print(f"You have {len(user.loans)} loans. They are:")
+        for loan in user.loans:
+            loan.pretty_print_loan(session, prefix=" " * 10)
+    if not user.loans:
+        print("You have no loans.")
+    print(f"Your status is {user.status}")
+
+    print("\nNotification center:")
+    notifications = []
+    if user.status == UserStatusSavingEnum.OVERDUE_LOANS:
+        notifications.append(
+            " * You have overdue loans for more than 12 months. "
+            "You cannot withdraw from savings account or take new loans.", )
+        notifications.append(
+            " * You have overdue loans. Please pay them during this month otherwise the account will be locked.")
+    if user.rate_adjustment_is_needed():
+        notifications.append(" * Due to your (or automatic) recent actions your savings interest rate will be "
+                             "adjusted at the begging of the next month.")
+    if not notifications:
+        print(" * You have no notifications.")
+    else:
+        print("\n".join(notifications))
+
+    print("-" * 10 + " End of your dashboard " + "-" * 10)
+
+
 def handle_user_deposit_money_action(user):
     deposit_amount = IOUtils.input_float("Enter the amount you want to deposit (at least €1 and at most €1 million): ",
                                          lower_bound=1, upper_bound=1_000_000)
@@ -40,14 +70,15 @@ def handle_user_take_a_loan_action(session: Session, user: User):
         print("Error: ", e)
 
 
-def handle_user_pay_a_loan_action(user):
+def handle_user_pay_a_loan_action(session: Session, user: User):
     if len(user.loans) == 0:
         print("You have no loans")
         return
     if user.savings_account.savings_amount <= 0:
         print("You have no money in your savings account. Thus it is not possible to pay a loan. Deposit money first.")
         return
-    loan_index = IOUtils.print_menu_and_return_choice(user.loans, "Choose the loan you want to pay: ")
+    loan_index = IOUtils.print_menu_and_return_choice(user.loans, "Choose the loan you want to pay: ",
+                                                      "Enter the ID of the loan you want to pay for: ")
     loan = user.loans[loan_index - 1]
 
     sum_to_pay_at_most = min(user.savings_account.savings_amount, loan.sum)
@@ -61,7 +92,7 @@ def handle_user_pay_a_loan_action(user):
         lower_bound=0.01, upper_bound=loan.sum)
 
     try:
-        user.pay_loan(loan, pay_amount)
+        user.pay_loan(session, loan, pay_amount)
     except ValueError as e:
         print("Error: ", e)
 
@@ -84,31 +115,20 @@ def handle_user_mode(session: Session):
 
     IOUtils.print_section(f"Hello, {user.full_name}!")
     while True:
-        print("-" * 10 + " Your dashboard " + "-" * 10)
-        print("Your " + str(user.savings_account))
-        if user.loans:
-            print(f"You have {len(user.loans)} loans. They are:")
-            for loan in user.loans:
-                loan.pretty_print_loan(session, prefix=" " * 10)
-        if not user.loans:
-            print("You have no loans.")
-        print(f"Your status is {user.status}")
-        print("-" * 10)
+        print_user_dashboard(user, session)
 
         user_chosen_action = None  # action that is cho
         if user.status == UserStatusSavingEnum.LOCKED:
             print("This account is locked. You can not do anything. The user was automatically logged out.")
             break
         elif user.status == UserStatusSavingEnum.OVERDUE_LOANS:
-            print("You have unpaid loans for more than 12 months. "
-                  "You cannot withdraw from savings account or take new loans.")
             action = IOUtils.print_menu_and_return_choice(
                 ["Deposit to savings account", "Pay a loan", "Log out"],
                 USER_MENU_PROMPT_MESSAGE)
             if action == 1:
                 user_chosen_action = UserActionEnum.DEPOSIT
             if action == 2:
-                user_chosen_action = UserActionEnum.WITHDRAW
+                user_chosen_action = UserActionEnum.PAY_A_LOAN
             if action == 3:
                 user_chosen_action = UserActionEnum.LOGOUT
         else:
@@ -133,7 +153,7 @@ def handle_user_mode(session: Session):
         elif user_chosen_action == UserActionEnum.TAKE_A_LOAN:
             handle_user_take_a_loan_action(session, user)
         elif user_chosen_action == UserActionEnum.PAY_A_LOAN:
-            handle_user_pay_a_loan_action(user)
+            handle_user_pay_a_loan_action(session, user)
 
         elif user_chosen_action == UserActionEnum.LOGOUT:
             print("Logging out...")
